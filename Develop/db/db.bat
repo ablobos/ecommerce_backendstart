c841be9bcb35701fd36ed9691b41bb6df07aa33e
Abdiel "A.B." Villalobos <ablobosportfolio@gmail.com>
2023-03-26 19:40:37 -0400

    updated db_bat

---
+++ Develop/db/db.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+SET BINPATH=
+
+SET SQLPATH= "C:\Users\nxtlo\Desktop\ecommerce-backend_starter"
+
+SET DB_NAME="test_db"
+
+SET DB_USER= "root"
+
+SET DB_PASSWORD= "Password#123"
+
+CD "%BINPATH%"
+
+mysql --user=%DB_USER% --password=%DB_PASSWORD% -e "drop database if exists %DB_NAME%; create database %DB_NAME%;"
+mysql --user=%DB_USER% --password=%DB_PASSWORD% %DB_NAME% < %SQLPATH%\schema.sql
+mysql --user=%DB_USER% --password=%DB_PASSWORD% -e "drop database if exists %DB_NAME%;"
